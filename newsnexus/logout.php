dda65dfe86bacd4c0469d0610f244620e4fa724f
palva39 <paa39@njit.edu>
2025-03-17 18:27:27 -0400

    adjusted logout to actually destroy session and not save users old data when loggin out

--- newsnexus/logout.php
+++ newsnexus/logout.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// Destroy the session
 session_destroy();
+
+// Remove session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+// Force browser cache to clear
+header("Cache-Control: no-cache, no-store, must-revalidate"); 
+header("Expires: 0");
+header("Pragma: no-cache");
+
+// Send JSON response
 header("Content-Type: application/json");
 echo json_encode(["status" => "success", "message" => "Logged out successfully"]);
 exit();
